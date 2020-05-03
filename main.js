2cf99e6f5d4b9483ebee569f79dd3195ad83bc75
Gio <giovanni.berl1@gmail.com>
2020-05-03 10:43:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $(".next").on("click", function () {
+        //IMMAGINI
+        var immagineCorrente = $(".active");
+        // immagineCorrente.fadeOut(2000);
+        immagineCorrente.removeClass("active");
+        var immagineSuccessiva = immagineCorrente.next("img");
+        //PALLINI
+        var pallinoCorrente = $(".rettangolo.fill");
+        pallinoCorrente.removeClass("fill");
+        var pallinoSuccessivo = pallinoCorrente.next();
+
+        if (immagineSuccessiva.length) {
+            immagineSuccessiva.addClass("active");
+            // immagineSuccessiva.fadeIn(2000);
+            pallinoSuccessivo.addClass("fill");
+        } else {
+            immagineSuccessiva = $(".slider-inner img:first-of-type");
+            immagineSuccessiva.addClass("active");
+            pallinoSuccessivo = $(".slider-inner .rettangolo:first-child");
+            pallinoSuccessivo.addClass("fill");
+        }
+    });
+
+    $(".prev").on("click", function () {
+        //IMMAGINI
+        var immagineCorrente = $(".active");
+        immagineCorrente.removeClass("active");
+        var immaginePrecedente = immagineCorrente.prev("img");
+        //PALLINI
+        var pallinoCorrente = $(".rettangolo.fill");
+        pallinoCorrente.removeClass("fill");
+        var pallinoPrecedente = pallinoCorrente.prev();
+        if (immaginePrecedente.length) {
+            immaginePrecedente.addClass("active");
+            pallinoPrecedente.addClass("fill");
+        } else {
+            immaginePrecedente = $(".slider-inner img:last-of-type");
+            immaginePrecedente.addClass("active");
+            pallinoPrecedente = $(".slider-inner .rettangolo:last-child");
+            pallinoPrecedente.addClass("fill");
+        }
+    });
+
+    //CLICK SUI PALLINI
+    $(".slider-inner .rettangolo").on("click", function () {
+        //PALLINI
+        $(".fill").removeClass("fill");
+        var indicePallino = $(this).index() + 1;
+        $(`.rettangolo:nth-child(${indicePallino})`).addClass("fill");
+        //IMMAGINI
+        var immagineCorrente = $(".slider-inner img.active");
+        immagineCorrente.removeClass("active");
+        var immagineVoluta = $(`.slider-inner img:nth-child(${indicePallino})`);
+        immagineVoluta.addClass("active");
+    });
+});
