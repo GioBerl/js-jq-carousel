c541726956dacaaefaae3736b050cf60d850b037
Gio <giovanni.berl1@gmail.com>
2020-05-04 19:26:02 +0200

    aggiunto scorrimento automatico e pulsanti start and stop (da migliorare)

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $(".next").on("click", function () {
+        clearInterval(clock);
         //IMMAGINI
         var immagineCorrente = $(".active");
         // immagineCorrente.fadeOut(2000);
@@ -23,6 +24,7 @@ $(document).ready(function () {
     });
 
     $(".prev").on("click", function () {
+        clearInterval(clock);
         //IMMAGINI
         var immagineCorrente = $(".active");
         immagineCorrente.removeClass("active");
@@ -44,6 +46,7 @@ $(document).ready(function () {
 
     //CLICK SUI PALLINI
     $(".slider-inner .rettangolo").on("click", function () {
+        clearInterval(clock);
         //PALLINI
         $(".fill").removeClass("fill");
         var indicePallino = $(this).index() + 1;
@@ -53,5 +56,55 @@ $(document).ready(function () {
         immagineCorrente.removeClass("active");
         var immagineVoluta = $(`.slider-inner img:nth-child(${indicePallino})`);
         immagineVoluta.addClass("active");
+    });
+
+    // SCORRI LE IMMAGINI
+    var clock = setInterval(function () {
+        //IMMAGINI
+        var immagineCorrente = $(".active");
+        immagineCorrente.removeClass("active");
+        var immagineSuccessiva = immagineCorrente.next("img");
+        //PALLINI
+        var pallinoCorrente = $(".rettangolo.fill");
+        pallinoCorrente.removeClass("fill");
+        var pallinoSuccessivo = pallinoCorrente.next();
+
+        if (immagineSuccessiva.length) {
+            immagineSuccessiva.addClass("active");
+            pallinoSuccessivo.addClass("fill");
+        } else {
+            immagineSuccessiva = $(".slider-inner img:first-of-type");
+            immagineSuccessiva.addClass("active");
+            pallinoSuccessivo = $(".slider-inner .rettangolo:first-child");
+            pallinoSuccessivo.addClass("fill");
+        }
+    }, 3000);
+
+    //BOTTONI START E STOP
+    $(".stop").click(function () {
+        clearInterval(clock);
+    });
+
+    $(".start").click(function () {
+        clock = setInterval(function () {
+            //IMMAGINI
+            var immagineCorrente = $(".active");
+            immagineCorrente.removeClass("active");
+            var immagineSuccessiva = immagineCorrente.next("img");
+            //PALLINI
+            var pallinoCorrente = $(".rettangolo.fill");
+            pallinoCorrente.removeClass("fill");
+            var pallinoSuccessivo = pallinoCorrente.next();
+
+            if (immagineSuccessiva.length) {
+                immagineSuccessiva.addClass("active");
+                pallinoSuccessivo.addClass("fill");
+            } else {
+                immagineSuccessiva = $(".slider-inner img:first-of-type");
+                immagineSuccessiva.addClass("active");
+                pallinoSuccessivo = $(".slider-inner .rettangolo:first-child");
+                pallinoSuccessivo.addClass("fill");
+            }
+        }, 3000);
     });
 });
